23d4b55080604b3cdacb914615d48dd6bc9b6922
Martin Wijaya <martinwijaya2013@gmail.com>
2021-11-08 10:35:41 +0700

    Init

---
+++ SmallFantasy/Assets/Script/EnemyAttribute.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttribute : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 20;
+                     private int hp;
+    [SerializeField] private int attack;
+    [SerializeField] private int exp;
+    [SerializeField] private int level;
+
+    public Animator animator;
+
+    //public HeroAttribute attribute;
+    // Start is called before the first frame update
+    void Start()
+    {
+        level = GameObject.Find("Player").GetComponent<HeroAttribute>().GetLevel();
+        maxHealth = 20 + (5 * level);
+        hp = maxHealth;
+        attack = 5 + (3 * level);
+        exp = 5 + (3 * level);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+
+        animator.SetTrigger("Hit");
+
+        if(hp <= 0)
+        {
+            die();
+        }
+    }
+
+    public void die()
+    {
+        animator.SetBool("Dead", true);
+
+        GetComponent<EnemyCombat>().enabled = false;
+        GetComponent<EnemyMovement>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+        StartCoroutine(DestroyThis());
+    }
+
+    IEnumerator DestroyThis()
+    {
+        yield return new WaitForSeconds(2);
+        
+        Destroy(gameObject);
+    }
+
+    public int GetHp()
+    {
+        return hp;
+    }
+
+    public int GetExp()
+    {
+        return exp;
+    }
+
+    public int GetAttack()
+    {
+        return attack;
+    }
+
+}
---
+++ SmallFantasy/Assets/Script/HeroMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class HeroMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Animator animator;
+
+    private float horizontalMove = 0f;
+
+    [SerializeField] private float movementSpeed = 5f;
+
+    private bool jump = false;
+    private bool move = true;
+
+    private void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("AttackAnimation"))
+        {
+            move = false;
+            horizontalMove = 0;
+        }
+        else
+        {
+            move = true;
+        }
+            
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("Jumping", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            animator.SetTrigger("Attacking");
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("Jumping", false);
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        if(move == true)
+        {
+            horizontalMove = Input.GetAxisRaw("Horizontal") * movementSpeed;
+        }
+        jump = false;
+    }
+
+}
